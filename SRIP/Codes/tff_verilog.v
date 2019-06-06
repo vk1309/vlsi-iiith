24284484c3040a692474f3506559027bb401ba97
vk1309 <39329373+vk1309@users.noreply.github.com>
2019-06-06 19:24:38 +0530

    Add files via upload

---
+++ SRIP/Codes/tff_verilog.v
@@ -0,0 +1,16 @@
+module tff(data,clk,reset,q)
+input data,clk,reset;
+output q;
+reg q;
+always @(posedge clk or negedge reset)
+begin
+if(~reset)
+begin
+q<=1'b0;
+end 
+else if(data)
+begin
+q<=!q;
+end
+end
+endmodule
