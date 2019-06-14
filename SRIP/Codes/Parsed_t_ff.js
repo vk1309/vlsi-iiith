1d9405d1e60fcfed0106995168575a0cd524f385
vk1309 <39329373+vk1309@users.noreply.github.com>
2019-06-14 14:58:28 +0530

    Parsed_t_ff waveform

---
+++ SRIP/Codes/Parsed_t_ff.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> POSITIVE EDGE TRIGERRED T FLIP FLOP</title>
+</head>
+<body onload="WaveDrom.ProcessAll()">
+<script src="http://wavedrom.com/skins/default.js" type="text/javascript"></script>
+<script src="http://wavedrom.com/wavedrom.min.js" type="text/javascript"></script>
+var ttf_verilog = require('./tff_verilog');
+var actions = {
+<script type="WaveDrom">
+{ signal: [
+  
+  { name: "T", wave: '1..0.10.1.010.1.010' },
+  { name: "Input", wave: '1..0.1.0..1..0...1.'},
+  { name: "Pclk", wave: 'P..................' },
+  { name: "Q", wave: '0.....101..0101.0.1'},
+]}
+</script>
+  [..]
+};
+</body>
+</html>
