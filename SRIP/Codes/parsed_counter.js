9f5f935ef146a224d3fda15a2defa12fc1bd8ac6
vk1309 <39329373+vk1309@users.noreply.github.com>
2019-06-14 17:43:30 +0530

    Add files via upload

---
+++ SRIP/Codes/parsed_counter.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> POSITIVE EDGE TRIGERRED T FLIP FLOP</title>
+</head>
+<body onload="WaveDrom.ProcessAll()">
+<script src="http://wavedrom.com/skins/default.js" type="text/javascript"></script>
+<script src="http://wavedrom.com/wavedrom.min.js" type="text/javascript"></script>
+var veri_counter = require('./veri_counter');
+var actions = {
+<script type="WaveDrom">
+{ signal: [
+  
+  { name: "Pclk", wave: 'P...................' },
+  { name:"OUTPUT", wave: 'x345345345345345345x', data: '0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7' },
+]}
+</script>
+  [..]
+};
+</body>
+</html>
